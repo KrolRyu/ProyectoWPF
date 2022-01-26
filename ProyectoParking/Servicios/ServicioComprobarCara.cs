c679d3a4d65579f73df87de5031570fb58fa33aa
Pablo <pablo.martinezaznar@iesdoctorbalmis.com>
2022-01-26 11:59:53 +0100

    Creo y acaboel servicio del blob de azure y empiezo a hacer el servico de Comprobar las caras

---
+++ ProyectoParking/Servicios/ServicioComprobarCara.cs
@@ -0,0 +1,32 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoParking.Servicios
+{
+    static class ServicioComprobarCara
+    {
+        public static void ComprobarCara(string imagen)
+        {
+            var response = PostCara(imagen);
+            string respuesta = response.Content;
+        }
+
+        public static IRestResponse PostCara(string imagen)
+        {
+            //Cambiar variables a variables de configuracion
+            var client = new RestClient("https://servicio-face-proyecto-parking.cognitiveservices.azure.com//face/v1.07/");
+            var request = new RestRequest("detect", Method.POST);
+            string data = "{ 'url':'[" + imagen + "]'}";
+            request.AddHeader("Ocp-Apim-Subscription-Key", "38cac79a9bd04f648466d0b24ad1d9f5");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("application/json", data, ParameterType.RequestBody);
+            request.AddParameter("returnFaceAttributes", "age,gender", ParameterType.QueryString);
+            var response = client.Execute(request);
+            return response;
+    }
+    }
+}
---
+++ ProyectoParking/Servicios/ServicioImgs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+
+namespace ProyectoParking.Servicios
+{
+    static class ServicioImgs
+    {
+        public static String SubirImagenAAzure(string rutaImagen)
+        {
+            try
+            {
+                //Cambiar a la configuracion del proyecto
+                string cadenaDeConexion = "DefaultEndpointsProtocol=https;AccountName=actividadtema5;AccountKey=Plt4O/P0i4coCkog+BsK/FeBXHnz6sTqtlfVW2dXCT8FIkwfO9teD7fCI5rSwbNau2dvcv3Sf9AxmkkUeOyW9A==;EndpointSuffix=core.windows.net";
+                string nombreContenedorBlobs = "imgs-proyecto-parking";
+
+                var clienteBlobService = new BlobServiceClient(cadenaDeConexion);
+                var clienteContenedor = clienteBlobService.GetBlobContainerClient(nombreContenedorBlobs);
+                Stream streamImagen = File.OpenRead(rutaImagen);
+                string nombreImagen = Path.GetFileName(rutaImagen);
+                clienteContenedor.UploadBlob(nombreImagen, streamImagen);
+
+                var clienteBlobImagen = clienteContenedor.GetBlobClient(nombreImagen);
+                string urlImagen = clienteBlobImagen.Uri.AbsoluteUri;
+                return urlImagen;
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+    }
+}
