d40b6334ba79e7923351720587ad4cda822819ce
KrolRyu <mario.martos@iesdoctorbalmis.com>
2022-01-31 13:59:54 +0100

    Botones Añadir Editar y Eliminar creados DataGrid creado (Falta linkear con DB)

--- ProyectoParking/vistas/Clientes.xaml.cs
+++ ProyectoParking/vistas/Clientes.xaml.cs
@@ -27,5 +27,20 @@ namespace ProyectoParking.vistas
             InitializeComponent();
             DataContext = vm;
         }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
