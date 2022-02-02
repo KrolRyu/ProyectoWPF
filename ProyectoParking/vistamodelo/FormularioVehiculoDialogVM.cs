93feb00beebe1556c6053ad3be8b7c3a643a4d71
Pablo <pablo.martinezaznar@iesdoctorbalmis.com>
2022-02-02 10:41:13 +0100

    Empiezo la VM de  ediar/crear vehiculo

---
+++ ProyectoParking/vistamodelo/FormularioVehiculoDialogVM.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using ProyectoParking.ClasesModelo;
+using ProyectoParking.Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoParking.vistamodelo
+{
+    class FormularioVehiculoDialogVM: ObservableObject
+    {
+        //Propiedades
+
+        private Vehiculo vehiculoActual;
+
+        public Vehiculo VehiculoActual
+        {
+            get { return vehiculoActual; }
+            set { SetProperty(ref vehiculoActual, value); }
+        }
+
+        private string fotoVehiculo;
+
+        public string FotoVehiculo
+        {
+            get { return fotoVehiculo; }
+            set { SetProperty(ref fotoVehiculo, value); }
+        }
+
+        //Contructor
+        public FormularioVehiculoDialogVM()
+        {
+            this.vehiculoActual = new Vehiculo();
+        }
+
+        //Metodos
+
+        public void SacarMatriculaYTipo()
+        {
+            //Cuando el servicio de subir imagen en fichero este hecho, recoger la imagen en la variable
+            FotoVehiculo = ServicioImgs.SubirImagenAAzure(FotoVehiculo);
+            if(FotoVehiculo != "")
+            {
+                //TODO sacar la matricula y el tipo
+                string matricula = "";
+                string tipo = "";
+                VehiculoActual.Tipo = tipo;
+                VehiculoActual.Matricula = matricula;
+            }
+            else
+            {
+                //TODO usar el servicio de mensajes para que diga que ha habido un error
+            }
+        }
+
+    }
+}
