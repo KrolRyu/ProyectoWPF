43dbde570f43190247521016e86da0f77c738bb9
Pablo <pablo.martinezaznar@iesdoctorbalmis.com>
2022-02-07 15:03:48 +0100

    Empiezo la ventana del doble click

---
+++ ProyectoParking/vistas/VerEstacionamientos.xaml.cs
@@ -0,0 +1,30 @@
+﻿using ProyectoParking.vistamodelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoParking.vistas
+{
+    /// <summary>
+    /// Lógica de interacción para VerEstacionamientos.xaml
+    /// </summary>
+    public partial class VerEstacionamientos : Window
+    {
+        VerEstacionamientosVM vm = new VerEstacionamientosVM();
+        public VerEstacionamientos()
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+    }
+}
